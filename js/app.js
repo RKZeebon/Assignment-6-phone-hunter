4827b995e7e940646822f411d3f5ab8f7b28fcd7
RKZeebon <rkzeebon@gmail.com>
2022-03-01 03:59:55 +0600

    edit some code

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
-/* Functions for Search Result */
+const noResult = document.getElementById('no-result');
+const container = document.getElementById('cards-container');
+const detailsContainer = document.getElementById('details');
 
+/* Functions for Search Result */
 const loadSearchResult = () => {
     const searchInput = document.getElementById('search-input');
     const searchText = searchInput.value.toLowerCase();
@@ -14,11 +17,11 @@ const loadSearchResult = () => {
 }
 
 const showSearchResult = (SearchResult) => {
-    const noResult = document.getElementById('no-result');
-    noResult.textContent = ""
 
-    const container = document.getElementById('cards-container');
+    noResult.textContent = ""
+    detailsContainer.textContent = '';
     container.textContent = ""
+
 
     if (SearchResult < 1) {
 
@@ -34,19 +37,16 @@ const showSearchResult = (SearchResult) => {
     else {
         const needToShow = SearchResult.slice(0, 20)
 
-        for (result of needToShow) {
+        for (const result of needToShow) {
             const newDiv = document.createElement('div');
             newDiv.classList.add('col');
 
-            newDiv.innerHTML = `<img src="${result.image}" class="card-img-top img-thumbnail" alt="..." style="height: 500px;">
-        <div class="card-body">
+            newDiv.innerHTML = `<img src="${result.image}" class="card-img-top img-thumbnail" alt="..." style="width: 250px;">
+            <div class="card-body">
             <h4 class="card-title">Phone Name: ${result.phone_name}</h4>
-            <h5 class="card-text">Brand Name: ${result.brand}</h5 >
-        </div >
-        <div class="card-footer ">
-        <button onclick="loaddetails('${result.slug}')" class="btn btn-primary w-100">See more details</button>
-        </div>`
-
+            <h5 class="card-text">Brand Name: ${result.brand}</h5 ></div >
+            <div class="card-footer ">
+            <button onclick="loaddetails('${result.slug}')" class="btn btn-primary w-100">See more details</button></div>`
 
             container.appendChild(newDiv);
         }
@@ -64,7 +64,7 @@ const loaddetails = (getId) => {
 
     fetch(url)
         .then(res => res.json())
-        .then(datas => showTheDetails(datas.data));
+        .then(result => showTheDetails(result.data));
 }
 
 
@@ -77,46 +77,47 @@ const showTheDetails = (details) => {
         sensorList.push(" " + sensor);
     }
 
-    const detailsContainer = document.getElementById('details');
+
     detailsContainer.textContent = '';
 
     const newDiv = document.createElement('div');
+    newDiv.classList.add('d-md-flex')
     newDiv.innerHTML = `
-    <div class="text-center">
-    <img src="${details.image}" alt="...">
+    <div class="text-center mb-5">
+    <img src="${details.image}" alt="..." style="width: 400px;">
     </div>
     <table class="table">
                 <tbody>
                     <tr>
-                        <td class="text-end"><b>Phone Name:</b></td>
-                        <td>${details.name}</td>
+                        <td class="text-end align-middle"><b>Phone Name:</b></td>
+                        <td class="align-middle">${details.name}</td>
                     </tr>
                     <tr>
-                        <td class="text-end"><b>Release Date:</b></td>
-                        <td>${details.releaseDate}</td>
+                        <td class="text-end align-middle"><b>Release Date:</b></td>
+                        <td class="align-middle">${details.releaseDate}</td>
                     </tr>
                     <tr>
-                        <td class="text-end"><h4>Main Features: </h4></td>
+                        <td class="text-end align-middle"><h4>Main Features: </h4></td>
                         <td></td>
                     </tr>
                     <tr>
-                        <td class="text-end"><b>Storage:</b></td>
-                        <td>${details.mainFeatures.storage}</td>
+                        <td class="text-end align-middle"><b>Storage:</b></td>
+                        <td class="align-middle">${details.mainFeatures.storage}</td>
                     </tr>
                     <tr>
-                        <td class="text-end"><b>Display Size:</b></td>
-                        <td>${details.mainFeatures.displaySize}</td>
+                        <td class="text-end align-middle"><b>Display Size:</b></td>
+                        <td class="align-middle">${details.mainFeatures.displaySize}</td>
                     </tr>
                     <tr>
-                        <td class="text-end"><b>Chip Set:</b></td>
-                        <td>${details.mainFeatures.chipSet}</td>
+                        <td class="text-end align-middle"><b>Chip Set:</b></td>
+                        <td class="align-middle">${details.mainFeatures.chipSet}</td>
                     </tr>
                     <tr>
-                        <td class="text-end"><b>Memory:</b></td>
-                        <td>${details.mainFeatures.memory}</td>
+                        <td class="text-end align-middle"><b>Memory:</b></td>
+                        <td class="align-middle">${details.mainFeatures.memory}</td>
                     </tr>
                     <tr>
-                        <td class="text-end"><h4>Sensors: </h4></td>
+                        <td class="text-end align-middle"><h4>Sensors: </h4></td>
                         <td class="align-middle">${sensorList}</td>
                     </tr>
                 </tbody>
